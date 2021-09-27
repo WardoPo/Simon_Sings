aa5bb3925eb247c50a1d08c6ffac1d492d32a709
Aaron Hidalgo Guerra <hidalguea@hotmail.com>
2021-09-26 20:05:54 -0500

    Singing First Draft
    Experiments on spectrum.
    First draft on play_simon.gd

--- play_simon.gd
+++ play_simon.gd
@@ -4,11 +4,17 @@ var max_levels = 20
 var current_level = 0
 
 const colors = ["Red","Yellow","Blue","Green"];
+const notes_FrBg = {"Do":261.63,"Re":293.665,"Mi":329.628,"Fa":349.228,"Sol":391.995,"La":440,"Si":493.883,"Do4":523.25}
 
 var colorOrder = [];
+var notesOrder = []
 var pressedOrder = [];
 
+var notes=[0,0,0,0,0,0,0,0]
+var notes_highest=[0,0,0,0,0,0,0,0]
+
 var has_won = false;
+var is_listening = false;
 var has_lost = false;
 
 export var winTone : AudioStream;
@@ -20,6 +26,9 @@ var RedButton;
 var YellowButton;
 var BlueButton;
 var GreenButton;
+var NoteLabel;
+
+var spectrum
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -28,11 +37,14 @@ func _ready():
 	YellowButton = find_node("YellowButton");
 	BlueButton = find_node("BlueButton");
 	GreenButton = find_node("GreenButton")
+	NoteLabel = find_node("Nota");
+	spectrum = AudioServer.get_bus_effect_instance(3,0)
 	_create_game();
 	#get_node("TextureButton").connect("button_down",self,"_on_TextureButton_button_down");
-
+	
 func _create_game():
 	colorOrder = [];
+	NoteLabel.text="";
 	randomize();
 	for m in max_levels:
 		colorOrder.append(colors[randi()%4])
@@ -40,9 +52,14 @@ func _create_game():
 
 func _start_level(level):
 	pressedOrder = []
+	notesOrder = []
 	current_level = level
+	
+	for m in current_level:
+		notesOrder.append(notes_FrBg.keys()[randi()%8])
+	
 	_show_level(current_level)
-		
+	$Timer.start(3.0)
 	print("Color Order:",colorOrder)
 
 func _show_level(level):
@@ -59,16 +76,37 @@ func _show_level(level):
 				yield(GreenButton.glow(),"completed")
 	_disable_all(false)
 
+func _show_note(note):
+	NoteLabel.text = note
+	yield(get_tree().create_timer(0.5),"timeout");
+	NoteLabel.text=""
+	
+
 func _check():
+	$Timer.stop()
 	print("PressedOrder:",pressedOrder)
 	var current_index = pressedOrder.size()-1
 	has_lost = pressedOrder[current_index] != colorOrder[current_index]
 	if has_lost:
 		_lose()
 		print("You Loose, Looser :c")
+		
+	elif !has_lost && !pressedOrder.size()==current_level :
+		yield(_show_note(notesOrder[pressedOrder.size()-1]),"completed")
+		is_listening = true
+		yield(get_tree().create_timer(3.0),"timeout")
+		is_listening = false
+		$Timer.start(3.0)
+		
 	if !has_lost && pressedOrder.size()==current_level:
 		_win()
-		
+
+func _check_note(note):
+	if note == notesOrder[pressedOrder.size()-1]:
+		$Timer.start(3.0)
+	else:
+		_lose()
+
 func _lose():
 	_disable_all(true)
 	yield(get_tree().create_timer(0.5), "timeout")
@@ -112,3 +150,19 @@ func _set_wrong_all(forward):
 func _on_button_hit(id):
 	pressedOrder.append(id)
 	_check()
+
+
+func _on_Timer_timeout():
+	_lose()
+
+func _process(delta):
+	if is_listening:
+		notes = [_in_tune_val("Do"),_in_tune_val("Re"),_in_tune_val("Mi"),_in_tune_val("Fa"),_in_tune_val("Sol"),_in_tune_val("La"),_in_tune_val("Si"),_in_tune_val("Do4")]
+		
+		for note in notes:
+			notes[notes.find(note)] = null if stepify(note,0.0001) == 0.000 else stepify(note,0.0001)
+			if notes[notes.find(note)] != null :
+				notes_highest[notes.find(note)] = notes_highest[notes.find(note)] if notes_highest[notes.find(note)] > notes[notes.find(note)] else notes[notes.find(note)]
+
+func _in_tune_val(var note):
+	return spectrum.get_magnitude_for_frequency_range(floor(notes_FrBg[note]),ceil(notes_FrBg[note])).length();
--- show_spectrum.gd
+++ show_spectrum.gd
@@ -8,24 +8,57 @@ const HEIGHT = 100
 
 const MIN_DB = 60
 
+const notes_FrBg = {"Do":261.63,"Re":293.665,"Mi":329.628,"Fa":349.228,"Sol":391.995,"La":440,"Si":493.883,"Do4":523.25}
+
 var spectrum
 
-func _draw():
-	#warning-ignore:integer_division
-	var w = WIDTH / VU_COUNT
-	var prev_hz = 0
-	for i in range(1, VU_COUNT+1):
-		var hz = i * FREQ_MAX / VU_COUNT;
-		var magnitude: float = spectrum.get_magnitude_for_frequency_range(prev_hz, hz).length()
-		var energy = clamp((MIN_DB + linear2db(magnitude)) / MIN_DB, 0, 1)
-		var height = energy * HEIGHT
-		draw_rect(Rect2(w * i, HEIGHT - height, w, height), Color.white)
-		prev_hz = hz
+var notes=[0,0,0,0,0,0,0,0]
 
+func _draw():
+	notes = [_in_tune_val("Do"),_in_tune_val("Re"),_in_tune_val("Mi"),_in_tune_val("Fa"),_in_tune_val("Sol"),_in_tune_val("La"),_in_tune_val("Si"),_in_tune_val("Do4")]
+	
+	for note in notes:
+		notes[notes.find(note)] = null if stepify(note,0.0001) == 0.000 else stepify(note,0.0001)
+	
+	if notes.max() == null:
+		get_node("Notes/Detected_Note").text = "UwU"
+#		print("Shhhhh")
+	else:
+		match notes.find(notes.max()):
+			0:
+				get_node("Notes/Detected_Note").text = "Do"
+#				print("Do")
+			1:
+				get_node("Notes/Detected_Note").text = "Re"
+#				print("Re")
+			2:
+				get_node("Notes/Detected_Note").text = "Mi"
+#				print("Mi")
+			3:
+				get_node("Notes/Detected_Note").text = "Fa"
+#				print("Fa")
+			4:
+				get_node("Notes/Detected_Note").text = "Sol"
+#				print("Sol")
+			5:
+				get_node("Notes/Detected_Note").text = "La"
+#				print("La")
+			6:
+				get_node("Notes/Detected_Note").text = "Si"
+#				print("Yes")
+			7:
+				get_node("Notes/Detected_Note").text = "Do"
+#				print("Do")
+	
+
+func _in_tune_val(var note):
+	return spectrum.get_magnitude_for_frequency_range(floor(notes_FrBg[note]),ceil(notes_FrBg[note])).length();
 
 func _process(_delta):
+	
 	update()
 
-
 func _ready():
-	spectrum = AudioServer.get_bus_effect_instance(0,0)
+#	spectrum = AudioServer.get_bus_effect_instance(0,0)
+	spectrum = AudioServer.get_bus_effect_instance(3,0)
+	
